cafeca227a198e56eb216900d05bd9c03642e880
Ritik Pant <pantritik2203@gmail.com>
2021-01-10 00:22:21 +0530

    first nodejs commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const http = require("http");
+
+// const server = http.createServer( (req,res) => {
+//     res.end("Result");
+// })
+
+// server.listen(8000, '127.0.0.1', ()=>{
+//     console.log("Listening");
+// });
+
+fs.writeFile("sec.js","", (err)=>{
+
+});
